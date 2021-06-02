54cde9cb3b89e572b08b7317f45f0720df9df3be
lingen.liu <lingen.liu@gmail.com>
2021-06-02 17:58:09 +0800

    feat: 升级myddd-vert.x版本至 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-//使用新的kotlin compile
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        useIR = true
-    }
-}
+
 
 plugins {
     java
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.10"
 }
 
 group = "org.myddd"
 version = "1.0.0-SNAPSHOT"
-extra["myddd_vertx_version"] = "1.0.0-SNAPSHOT"
-extra["vertx_version"] = "4.0.3"
+
+extra["myddd_vertx_version"] = "1.1.0-SNAPSHOT"
+extra["vertx_version"] = "4.1.0"
 extra["version"] = version
 
 extra["log4j_version"] = "2.14.0"
@@ -23,7 +19,9 @@ extra["jackson_version"] = "2.12.1"
 extra["javax_persistence_version"] = "2.2.1"
 extra["mockito_version"] = "3.7.7"
 extra["junit5_version"] = "5.7.1"
-extra["hibernate_core_version"] = "5.4.28.Final"
+
+extra["hibernate_reactive_version"] = "1.0.0.CR6"
+extra["hibernate_core_version"] = "5.5.0.Final"
 
 allprojects {
     repositories {
@@ -47,12 +45,6 @@ allprojects {
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
-    }
-
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            useIR = true
-        }
     }
 }
 
--- myddd-starter-infra/build.gradle.kts
+++ myddd-starter-infra/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     api("org.myddd.vertx:myddd-vertx-repository-hibernate:${rootProject.extra["myddd_vertx_version"]}")
 
     //api implementation
-    api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    api("org.hibernate.reactive:hibernate-reactive-core:${rootProject.extra["hibernate_reactive_version"]}")
     api("org.myddd.vertx:myddd-vertx-base-provider:${rootProject.extra["myddd_vertx_version"]}")
     implementation("org.apache.logging.log4j:log4j-core:${rootProject.extra["log4j_version"]}")
     implementation("com.fasterxml.jackson.core:jackson-databind:${rootProject.extra["jackson_version"]}")
